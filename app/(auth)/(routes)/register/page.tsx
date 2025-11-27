a072172c2a364944db788ec6da64beb3b2ff202e
jabjubinski <jabajnldz@gmail.com>
2025-11-28 01:28:55 +0400

    sorted routes, organised folders, added auth and middleware

---
+++ app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+  return (
+    <>
+      <p>register page!</p>
+    </>
+  );
+};
+
+export default RegisterPage;
---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full bg-red-500 text-5xl flex justify-center items-center flex-col">
+      This is a protected route
+    </div>
+  );
+}
